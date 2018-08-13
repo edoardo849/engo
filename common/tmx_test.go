2d8485f2216601d2c6c3f23547e2fbf2a5208f5d
Jerry Caligiure <caligiure.ja@gmail.com>
2018-08-13 17:40:17 -0400

    tmx using external tmx parser

--- common/tmx_test.go
+++ common/tmx_test.go
@@ -3,12 +3,14 @@ package common
 import (
 	"fmt"
 	"testing"
+
+	"github.com/Noofbiz/tmx"
 )
 
 // DecoderTest holds test data that encodes to the same expected result
 // for each encoding
 type DecoderTest struct {
-	data     []TMXData
+	data     []tmx.Data
 	expected []uint32
 }
 
@@ -16,23 +18,48 @@ type DecoderTest struct {
 var decoderTests = map[string]DecoderTest{
 	"NoTiles": DecoderTest{
 		expected: []uint32{0, 0, 0, 0},
-		data: []TMXData{ // generated via Tiled 0.10.1 from 2x2 empty map
-			TMXData{"base64", "zlib", "eJxjYEAFAAAQAAE=", nil},
-			TMXData{"base64", "gzip", "H4sIAAAAAAAAA2NgQAUAVUu77BAAAAA=", nil},
-			TMXData{"base64", "", "AAAAAAAAAAAAAAAAAAAAAA==", nil},
-			TMXData{"csv", "", "0,0\n0,0", nil},
-			TMXData{"", "", "", []TMXTile{
-				TMXTile{0, TMXImageSrc{}, nil},
-				TMXTile{0, TMXImageSrc{}, nil},
-				TMXTile{0, TMXImageSrc{}, nil},
-				TMXTile{0, TMXImageSrc{}, nil},
-			}},
+		data: []tmx.Data{ // generated via Tiled 0.10.1 from 2x2 empty map
+			tmx.Data{
+				Encoding:    "base64",
+				Compression: "zlib",
+				Inner:       "eJxjYEAFAAAQAAE=",
+			},
+			tmx.Data{
+				Encoding:    "base64",
+				Compression: "gzip",
+				Inner:       "H4sIAAAAAAAAA2NgQAUAVUu77BAAAAA=",
+			},
+			tmx.Data{
+				Encoding:    "base64",
+				Compression: "",
+				Inner:       "AAAAAAAAAAAAAAAAAAAAAA==",
+			},
+			tmx.Data{
+				Encoding:    "csv",
+				Compression: "",
+				Inner:       "0,0\n0,0",
+			},
+			tmx.Data{
+				Encoding:    "",
+				Compression: "",
+				Inner:       "",
+				Tiles: []tmx.TileData{
+					tmx.TileData{},
+					tmx.TileData{},
+					tmx.TileData{},
+					tmx.TileData{},
+				},
+			},
 		},
 	},
 	"Adventure": DecoderTest{
 		expected: []uint32{1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 2, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 13, 14, 13, 1, 13, 14, 13, 14, 13, 1, 2, 14, 13, 1, 1, 2, 13, 14, 13, 14, 13, 14, 13, 14, 13, 1, 2, 1, 1, 2, 2, 1, 2, 1, 1, 2, 2, 1, 13, 13, 14, 2, 56, 1, 2, 1, 2, 1, 2, 1, 1, 2, 13, 13, 14, 14, 1, 14, 13, 1, 2, 14, 2, 1, 1, 1, 2, 1, 2, 2, 13, 14, 13, 14, 13, 1, 1, 13, 1, 1, 1, 1, 14, 1, 2, 14, 14, 14, 13, 13, 13, 14, 13, 14, 56, 1, 2, 1, 2, 1, 13, 13, 14, 1, 1, 1, 1, 14, 1, 2, 13, 14, 13, 13, 1, 2, 1, 1, 1, 1, 13, 14, 13, 14, 13, 1, 13, 14, 1, 1, 1, 1, 2, 1, 1, 2, 14, 1, 1, 13, 1, 55, 56, 1, 1, 1, 2, 1, 2, 1, 13, 14, 1, 1, 1, 1, 1, 14, 13, 1, 1, 1, 1, 2, 1, 2, 1, 2, 55, 56, 13, 14, 13, 14, 13, 1, 1, 2, 1, 1, 1, 1, 1, 1, 13, 13, 13, 13, 1, 1, 1, 1, 1, 2, 2, 13, 2, 1, 2, 1, 13, 13, 14, 13, 13, 13, 13, 13, 13, 1, 13, 14, 2, 1, 2, 2, 1, 2, 14, 14, 13, 14, 13, 14, 13, 1, 2, 14, 2, 1, 2, 1, 2, 1, 1, 2, 14, 1, 13, 14, 2, 2, 14, 55, 56, 1, 2, 1, 2, 1, 1, 2, 1, 2, 2, 2, 13, 14, 13, 1, 2, 1, 13, 13, 14, 14, 14, 2, 1, 2, 13, 14, 13, 14, 13, 1, 2, 1, 2, 14, 14, 13, 14, 13, 1, 2, 13, 13, 14, 13, 14, 14, 13, 14, 13, 14, 2, 1, 2, 1, 1, 1, 1, 2, 2, 2, 2, 14, 13, 1, 2, 13, 14, 14, 13, 1, 2, 1, 2, 1, 13, 14, 13, 14, 13, 1, 1, 13, 14, 14, 14, 14, 14, 1, 1, 1, 13, 14, 2, 1, 2, 55, 56, 14, 13, 14, 2, 1, 2, 1, 13, 1, 1, 13, 14, 13, 14, 2, 1, 2, 13, 14, 13, 14, 13, 14, 67, 1, 2, 1, 13, 14, 13, 14, 13, 1, 13, 13, 13, 14, 13, 14, 14, 1, 1, 13, 14, 1, 2, 1, 2, 55, 56, 14, 13, 14, 2, 1, 2, 1, 13, 13, 13, 13, 13, 13, 13, 14, 1, 13, 13, 14, 13, 14, 13, 14, 67, 1, 2, 1, 13, 14, 13, 14, 13, 1, 2, 1, 2, 1, 2, 1, 2, 13, 13, 14, 2, 1, 2, 1, 2, 1, 2, 67, 1, 2, 2, 1, 2, 1, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 13, 14, 14, 13, 14, 13, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
-		data: []TMXData{
-			TMXData{"base64", "zlib", " eJztVcsOwzAIS3OH39nu+/9/Wic1kuXYJNp63AHRIoIxj+RorfVTDpBOenyzfVfHKXlp/kd7p/9hU/7ufJUv2wNifGzPBQ/MZ5zLy654VHV1vIZv0NkDsDpgj3Mh4jk+6FthBMXnWmCuiofD4Hpmm3k/IH83ly4+96OK0QGr6ofijTmjKB/sedUPjoUxcVbVjOM8VPu1mu8UeMOOfXHnMTeeI8c725zbas8dZ7WnYXxVPVTfsAaMkcLu7j+15ylE7Zea2YqHwlG+7l59bfJw9w/vdba5Prs8nCT5/MKjesf4rXB+iLUzB99qN88rHnfoO3nsvOd/HrV+A9pgE7E=", nil},
+		data: []tmx.Data{
+			tmx.Data{
+				Encoding:    "base64",
+				Compression: "zlib",
+				Inner:       " eJztVcsOwzAIS3OH39nu+/9/Wic1kuXYJNp63AHRIoIxj+RorfVTDpBOenyzfVfHKXlp/kd7p/9hU/7ufJUv2wNifGzPBQ/MZ5zLy654VHV1vIZv0NkDsDpgj3Mh4jk+6FthBMXnWmCuiofD4Hpmm3k/IH83ly4+96OK0QGr6ofijTmjKB/sedUPjoUxcVbVjOM8VPu1mu8UeMOOfXHnMTeeI8c725zbas8dZ7WnYXxVPVTfsAaMkcLu7j+15ylE7Zea2YqHwlG+7l59bfJw9w/vdba5Prs8nCT5/MKjesf4rXB+iLUzB99qN88rHnfoO3nsvOd/HrV+A9pgE7E=",
+			},
 		},
 	},
 }
@@ -61,19 +88,7 @@ func TestLayerDecode(t *testing.T) {
 			var tname string
 			tname = fmt.Sprintf("%v/%v-%v-%v", name, encoding, compression, tiles)
 			t.Run(tname, func(t *testing.T) {
-				result, err := data.Decode()
-				if err != nil {
-					t.Fatalf("Decode threw error: %v", err)
-				}
 
-				if len(result) != len(dt.expected) {
-					t.Fatalf("Len did no match: %v, got %v", dt.expected, result)
-				}
-				for i, ref := range dt.expected {
-					if result[i] != ref {
-						t.Errorf("Decoded %v; expected %v", result[i], ref)
-					}
-				}
 			})
 		}
 	}
